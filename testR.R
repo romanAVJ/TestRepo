bb5bcf08d303598b41ccfde16d2f0c64cdba7888
Roman Alberto Velez Jimenez <rvelezji@itam.mx>
2020-04-12 18:03:14 -0500

    Excersises of branch1

--- testR.R
+++ testR.R
@@ -4,10 +4,51 @@
 #General tenmplate
 Hello my name is roman
 
-The name of the project is: Test1
+The name of the project is: Test1. Branch1 rocks!
 
 
 Date: 12/04/2020
+
+
+
+# Ejercicio 3
+Aqui es como si hicieramos un ejercicio de prueba y no mms esta quedando increible.
+noodoao
+ajjofr
+"#"
+cjiaofjo
+qw
+a
+fknoañ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Ejercicio 5
+dajoñwfj
+fretre
+w
+
+fwre
+wer
+fwer
+gw
+reg
 
 
 
