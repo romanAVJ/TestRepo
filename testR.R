f6d87f742699019683969b40c0df9f3d36384828
Roman Alberto Velez Jimenez <rvelezji@itam.mx>
2020-04-12 17:56:52 -0500

    General template ok!

---
+++ testR.R
@@ -0,0 +1,24 @@
+# Test to making and resolving conflicts. This is for the sake of the itam homeworks.
+
+
+#General tenmplate
+Hello my name is roman
+
+The name of the project is: Test1
+
+
+Date: 12/04/2020
+
+
+
+
+
+
+
+
+
+
+
+
+
+
