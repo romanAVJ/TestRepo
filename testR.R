826a561ebd373aecbca646b2b7abef951ced8e19
Roman Alberto Velez Jimenez <rvelezji@itam.mx>
2020-04-12 18:07:22 -0500

    Branch2 ok!

--- testR.R
+++ testR.R
@@ -4,10 +4,50 @@
 #General tenmplate
 Hello my name is roman
 
-The name of the project is: Test1
+The name of the project is: Test1 y Test2
 
 
 Date: 12/04/2020
+
+
+#Ejercicio 1. Branch2
+hdjlwwaflrf
+rfrewgwetg
+wtgtgtr
+23542436
+vfshgd
+
+ruryuy
+
+
+
+
+
+
+nfjhfuiwerhi
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Ejercicio 3. Branch2
+hiqwehurielf
+gwertwertrewt
+w
+ertg
+wert
+wetewrg
+ggwerbuefhqwuo
+bubuu
+
 
 
 
